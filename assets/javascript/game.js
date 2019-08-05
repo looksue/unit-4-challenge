a6f751820332a1e1b40155adffee30efd9695a6a
Sue Look <suelook1@gmail.com>
2019-08-05 13:42:57 -0400

    added vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,14 @@
 // declare variables
-var totalScore = 0;
-var randomNumber = 0;
-var wins = 0;
-var losses = 0;
-var randomCrystal1 = 0;
-var randomCrystal2 = 0;
-var randomCrystal3 = 0;
-var randomCrystal4 = 0;
-var justwonorlost = "";
+var mychar=0;
+var myopponnent = 0;
+var win = 0;
+var loss = 0;
+var captain = 0;
+var doc = 0;
+var gopher = 0;
+var isaac = 0;
+var julie = 0;
+var vicki=0;
+var mysplash=0;
+var oppsplash=0;
 
-function initializeGame() {
-    // set variables
-    randomCrystal1 = Math.floor(Math.random() * 12) + 1;
-    randomCrystal2 = Math.floor(Math.random() * 12) + 1;
-    randomCrystal3 = Math.floor(Math.random() * 12) + 1;
-    randomCrystal4 = Math.floor(Math.random() * 12) + 1;
-
-    randomNumber = Math.floor(Math.random() * 102) + 19;
-    totalScore = 0;
-    // display game status
-    $("#jewelrandomnumber").text(randomNumber);
-    $("#jeweltotalscore").text(totalScore);
-
-    if (justwonorlost === "") {
-        $("#jewelscores").html("<p>Wins: " + wins + "</p><p>Losses: " + losses + "</p>");
-    }
-    if (justwonorlost === "won") {
-        $("#jewelscores").html("<p>You won!</p><p>Wins: " + wins + "</p><p>Losses: " + losses + "</p>");
-    }
-    if (justwonorlost === "lost") {
-        $("#jewelscores").html("<p>You lost!</p><p>Wins: " + wins + "</p><p>Losses: " + losses + "</p>");
-    }
-    justwonorlost = "";
-}
-
-$(".crystal-image").on("click", function (event) {
-
-    // add the value of the crystal to totalscore
-    if (event.target.id === "crystal1") {
-        totalScore = totalScore + randomCrystal1;
-    }
-    if (event.target.id === "crystal2") {
-        totalScore = totalScore + randomCrystal2;
-    }
-    if (event.target.id === "crystal3") {
-        totalScore = totalScore + randomCrystal3;
-    }
-    if (event.target.id === "crystal4") {
-        totalScore = totalScore + randomCrystal4;
-    }
-
-    // display totalscore
-
-    $("#jeweltotalscore").text(totalScore);
-
-    // compare totalscore to randomnumber
-
-    if (totalScore < randomNumber) {        // keep playing
-        return;
-    };
-
-    if (totalScore === randomNumber) {      // winner
-        wins = wins + 1;
-        justwonorlost = "won";        
-        initializeGame();
-    };
-
-    if (totalScore > randomNumber) {        // loser
-        losses = losses + 1; 
-        justwonorlost = "lost";
-        initializeGame();
-    };
-
-})
-
-initializeGame();
